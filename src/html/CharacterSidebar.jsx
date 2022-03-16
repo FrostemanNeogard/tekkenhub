d03d9329aee50d415ee69b465c9204a5f9592f0e
Funnyorangcat <liamfrostemanneogard@gmail.com>
2022-03-16 13:19:24 +0100

    Added initial character sidebar code

--- src/html/CharacterSidebar.jsx
+++ src/html/CharacterSidebar.jsx
@@ -2,14 +2,55 @@ import '../stylesheets/CharacterSidebar.css';
 
 function CharacterSidebar() {
     return (
-
-        <section className="character-siderbar">
+        <section className="character-sidebar">
         
-            <h1>hi :)</h1>
+            <GetCharacterPanels />
 
         </section>
-
     )
+}
+
+// Function to create an article for each character including an image and a heading
+function GetCharacterPanels() {
+
+    let returnHTML = [];
+
+    // TEMPORARY way of getting an array of all characters
+    let characterArray = GetCharacters();
+
+    // Get all images from ../images/character_panels
+    let characterImages = ImportAll(require.context('./../images/character_panels', false, /\.png$/));
+
+    // For each character, create a character-panel article for them
+    for (let i = 0; i < characterArray.length; i++) {
+        returnHTML.push (
+            <article className="character-panel" key={`character-panel-${i}`}>
+
+                <img src={characterImages[i]} alt={characterArray[i]} />
+                <h2>{characterArray[i]}</h2>
+
+            </article>
+        )
+    }
+
+    // Return the resulting HTML code
+    return returnHTML;
+}
+
+// (currently temporary) function to get the names of all characters
+// which is used in the creation of their panels in the sidebar
+function GetCharacters() {
+    return [
+        'Bryan',
+        'Bob',
+        'Chloe',
+        'Lee'
+    ]
+}
+
+// Function to get all character panels from the character_panels folder
+function ImportAll(r) {
+    return r.keys().map(r);
 }
 
 export default CharacterSidebar;
