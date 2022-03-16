33cdc7522774377fa2eb9956799e031b62f697da
Funnyorangcat <liamfrostemanneogard@gmail.com>
2022-03-16 11:19:47 +0100

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './stylesheets/App.css';
+import CharacterSidebar from './html/CharacterSidebar';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <CharacterSidebar />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/html/CharacterSidebar.jsx
@@ -0,0 +1,15 @@
+import '../stylesheets/CharacterSidebar.css';
+
+function CharacterSidebar() {
+    return (
+
+        <section className="character-siderbar">
+        
+            <h1>hi :)</h1>
+
+        </section>
+
+    )
+}
+
+export default CharacterSidebar;
