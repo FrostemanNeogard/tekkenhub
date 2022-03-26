4772aa6e977edccebca2b66e5fd8df69cffd95e9
Funnyorangcat <Funnyorangcat>
2022-03-26 12:34:19 +0100

    Started working on mobile onClick fix

--- src/jsx/character_pages/Bryan.jsx
+++ src/jsx/character_pages/Bryan.jsx
@@ -26,7 +26,7 @@ class Bryan extends Component {
         return (
             <section className='character-guide'>
                 <CharacterTopbar func={this.changeState} displayingFrames={this.state.displayFrames}/>
-                <Introduction />
+                <Overview />
                 <Playstyle />
                 <Punishment />
                 <Combos />
@@ -66,10 +66,10 @@ function CharacterTopbar(props) {
 
 }
 
-function Introduction() {
+function Overview() {
     return (
-        <article className="character-info introduction">
-            <h2>Bryan Fury</h2>
+        <article className="character-info overview">
+            <h2>Overview</h2>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur voluptates odit at, aperiam cumque dignissimos in vitae officiis assumenda. Doloremque laborum suscipit quaerat, nisi quibusdam quia quae ullam aperiam quas sunt! Quos assumenda autem asperiores dignissimos laudantium. Id, laboriosam obcaecati quidem sunt deserunt temporibus? Reprehenderit vero est a ratione veritatis magni sit suscipit eaque eius repellendus numquam voluptas pariatur, temporibus iste earum nulla rem nam voluptatibus dignissimos beatae, aut dicta placeat. Minus quibusdam optio iusto nemo necessitatibus. Facere eos beatae adipisci natus sequi, unde odit similique! Nisi doloribus tempore laudantium? Expedita, consequatur soluta non delectus voluptate beatae quae esse ullam.</p>
         </article>
     )
