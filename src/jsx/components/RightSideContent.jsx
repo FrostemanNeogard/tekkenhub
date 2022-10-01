08ec289a9e55f930b63ca79c0759b6689c11c10b
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2022-10-01 15:14:10 +0200

    Minor cleanup

--- src/jsx/components/RightSideContent.jsx
+++ src/jsx/components/RightSideContent.jsx
@@ -1,7 +1,6 @@
 import '../../stylesheets/RightSideContent.css'
 import HomePage from '../components/HomePage';
 import Bryan from '../character_pages/Bryan';
-import DevilJin from '../character_pages/DevilJin';
 import { Component } from 'react';
 
 class RightSideContent extends Component {
@@ -17,7 +16,6 @@ function GetSelectedCharacter(props) {
     switch(props.name) {
         case 'home': return <HomePage />;
         case 'Bryan': return <Bryan />;
-        case 'Devil Jin': return <DevilJin />;
 
         default:
             return <h1>This page does not exist yet.</h1>
