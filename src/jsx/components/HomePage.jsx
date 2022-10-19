49252e9deef46f13c8d91e407d29ab6835864051
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2022-10-19 08:06:01 +0200

    Set up GitHub Pages

--- src/jsx/components/HomePage.jsx
+++ src/jsx/components/HomePage.jsx
@@ -5,11 +5,12 @@ class HomePage extends Component {
     render(){
         return (
             <section className='home-page'>
-                {/* <img className='tekken7-banner' src="http://static1.1.sqspcdn.com/static/f/721581/26174813/1430119853417/img_popbanner.jpg?token=Nxaq4dIsO%2F0bD77efv5%2By2Ungzk%3D" alt="Tekken 7 Banner" /> */}
                 <h1>Welcome to Tekken Hub!</h1>
 
                 <p>
-                    This is meant to be used as a hub for learning 
+                    This is meant to be used as a learning resource for the Tekken games. However, 
+                    if you're seeing this as of now 
+                    then it's most likely because you're looking to hire me as a web developer.
                 </p>
 
                 <p>
@@ -17,12 +18,6 @@ class HomePage extends Component {
                     development. It is constantly being worked on and the full project can be found 
                     on <a href='https://github.com/FrostemanNeogard/tekkenwebsitev2' target='_blank' rel='noreferrer'>GitHub</a>.
                 </p>
-
-                {/* <p>
-                    The frame data on this site has been taken 
-                    from <a href="https://geppopotamus.info/game/tekken7fr/index_en.htm" target="_blank" rel="noreferrer">Geppopotamus</a> and
-                     all credit goes to them.
-                </p> */}
 
             </section>
 
