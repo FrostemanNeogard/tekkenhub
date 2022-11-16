64d27036a11ed762f5a7fa0462ff620c77bb0905
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2022-11-16 01:30:44 +0100

    Clean up code

--- src/jsx/components/ComboSection.jsx
+++ src/jsx/components/ComboSection.jsx
@@ -19,13 +19,11 @@ class ComboSection extends Component {
         // Varuable that holds all HTML which gets returned in the end
         let returnHTML = [];
 
-        // Loop through each launcher to create HTML blocks for 
-        // each launcher
-        // --- This code is honestly sort of a mess and I should really fix it ---
+        // Loop through each launcher to create HTML blocks for each launcher
         for (let i = 0; i < comboData.length; i++) {
 
             // Launcher header
-            // Only adds a attribute if there is a combo launcher demo
+            // Only adds hyperlink if there is a combo launcher demo
             returnHTML.push (
                 <h3 key={`launcher-${i}`}>
                     {comboData[i].launcherDemo ? (
@@ -38,8 +36,7 @@ class ComboSection extends Component {
 
             // Loop through each combo for the current launcher
             for (let x = 0; x < comboData[i].routes.length; x++) {
-                // Format the combo notation to give the proper 
-                // CSS classes to the displayed notation
+                // Format the combo notation to give the proper CSS classes to the displayed notation
                 let seperatedNotation = comboData[i].routes[x].graphicalNotation.split(',');
                 let currentComboNotation = [];
                 for (let y = 0; y < seperatedNotation.length; y++) {
@@ -47,10 +44,6 @@ class ComboSection extends Component {
                         <span key={`launcher-${i}-combo-${x}-notation-${y}`} className={`tn tn-${seperatedNotation[y]}`}/>
                     )
                 }
-
-                // Check if there are notes for the current combo
-                // This is used under the "damage" note for each combo
-                let comboDamage = (!comboData[i].routes[x].damage ? "Unknown" : comboData[i].routes[x].damage) 
 
                 returnHTML.push(
                     <section key={`launcher-${i}-combo-${x}`}>
