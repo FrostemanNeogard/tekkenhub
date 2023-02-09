c779a80b5a3f731c04361f64a3d965b36aa6a876
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2023-02-09 02:20:18 +0100

    Change to functional component

--- src/jsx/components/HomePage.jsx
+++ src/jsx/components/HomePage.jsx
@@ -1,26 +1,21 @@
-import { Component } from 'react';
 import '../../stylesheets/HomePage.css';
 
-class HomePage extends Component {
-    render(){
-        return (
-            <section className='home-page'>
-                <h1>Welcome to Tekken Hub!</h1>
+export default function HomePage() {
+    return (
+        <section className='home-page'>
+            <h1>Welcome to Tekken Hub!</h1>
 
-                <p>
-                    This is currently a work in progress site which is quite early in 
-                    development. It is constantly being worked on and the full project can be found 
-                    on <a href='https://github.com/FrostemanNeogard/tekkenhub' target='_blank' rel='noreferrer'>GitHub</a>.
-                </p>
+            <p>
+                This is currently a work in progress site which is quite early in 
+                development. It is constantly being worked on and the full project can be found 
+                on <a href='https://github.com/FrostemanNeogard/tekkenhub' target='_blank' rel='noreferrer'>GitHub</a>.
+            </p>
 
-                <p>
-                    <span style={{fontWeight: "bold"}}>To get started</span>, open up the Character List by pressing it and then pick a character to view.
-                </p>
+            <p>
+                <span style={{fontWeight: "bold"}}>To get started</span>, open up the Character List by pressing it and then pick a character to view.
+            </p>
 
-            </section>
+        </section>
 
-        )
-    }
+    )
 }
-
-export default HomePage;
