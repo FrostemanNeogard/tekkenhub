e03675d4d5898049d10721f7d0386b5a683e8b26
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2023-02-09 02:18:26 +0100

    Change home page wording

--- src/jsx/components/HomePage.jsx
+++ src/jsx/components/HomePage.jsx
@@ -8,15 +8,13 @@ class HomePage extends Component {
                 <h1>Welcome to Tekken Hub!</h1>
 
                 <p>
-                    This is meant to be used as a learning resource for the Tekken games. However, 
-                    if you're seeing this as of now 
-                    then it's most likely because you're looking to hire me as a web developer.
-                </p>
-
-                <p>
                     This is currently a work in progress site which is quite early in 
                     development. It is constantly being worked on and the full project can be found 
                     on <a href='https://github.com/FrostemanNeogard/tekkenhub' target='_blank' rel='noreferrer'>GitHub</a>.
+                </p>
+
+                <p>
+                    <span style={{fontWeight: "bold"}}>To get started</span>, open up the Character List by pressing it and then pick a character to view.
                 </p>
 
             </section>
